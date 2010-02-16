5e9f321daf505fd2ddd0308a222c2ebe1e4863b4
holgerbrandl <holgerbrandl@653ba4c6-162f-11df-b3c2-2971b5c2da17>
2010-02-16 15:41:40 +0000

    multiple input for operators

--- trunk/src/de/mpicbg/tds/rm/rplugin/operators/OpenInROperator.java
+++ trunk/src/de/mpicbg/tds/rm/rplugin/operators/OpenInROperator.java
@@ -1,17 +1,16 @@
 package de.mpicbg.tds.rm.rplugin.operators;
 
-import com.rapidminer.example.ExampleSet;
+import com.rapidminer.operator.IOObject;
+import com.rapidminer.operator.Operator;
 import com.rapidminer.operator.OperatorDescription;
 import com.rapidminer.operator.OperatorException;
-import com.rapidminer.operator.io.AbstractExampleSetWriter;
-import com.rapidminer.parameter.ParameterType;
-import com.rapidminer.parameter.ParameterTypeString;
+import com.rapidminer.operator.ports.InputPortExtender;
 import de.mpicbg.tds.rm.rplugin.RUtils;
 import org.rosuda.REngine.REXP;
-import org.rosuda.REngine.RList;
 import org.rosuda.REngine.Rserve.RConnection;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 
@@ -20,32 +19,35 @@ import java.util.List;
  *
  * @author Holger Brandl
  */
-public class OpenInROperator extends AbstractExampleSetWriter {
+public class OpenInROperator extends Operator {
 
-	private static final String OUTPUT_VAR_NAME = "outputVarName";
 
 //    private InputPort exampleSetInput = getInputPorts().createPort("example set input", true);
+	private InputPortExtender inputs = new InputPortExtender("input table", getInputPorts());
 
 
 	public OpenInROperator(OperatorDescription description) {
 		super(description);
+
+		inputs.start();
 	}
 
 
-	@Override
-	public ExampleSet write(ExampleSet exampleSet) throws OperatorException {
+	public void doWork() throws OperatorException {
+
 		try {
 			RConnection connection = RUtils.createConnection();
 
 			// 1) convert exampleSet ihnto data-frame and put into the r-workspace
-			RList inputAsRList = RUtils.convert2RList(exampleSet);
-			String parName = getParameter(OUTPUT_VAR_NAME);
-			connection.assign(parName, REXP.createDataFrame(inputAsRList));
+			List<IOObject> inputs = this.inputs.getData(true);
 
+
+			List<String> parNames = RUtils.push2R(connection, inputs);
 			// save the work-space to a temporary file and open R
-			File tmpFile = File.createTempFile(parName, ".RData");
+			File tmpFile = File.createTempFile("rplugin", ".RData");
 
-			connection.eval("save(" + parName + ",file=\"" + tmpFile.getAbsolutePath().replace("\\", "/") + "\") ");
+			String allParams = parNames.toString().replace("[", "").replace("]", "").replace(" ", "");
+			connection.eval("save(" + allParams + ",file=\"" + tmpFile.getAbsolutePath().replace("\\", "/") + "\") ");
 			connection.eval("rm(list = ls(all = TRUE));");
 
 			connection.close();
@@ -54,6 +56,16 @@ public class OpenInROperator extends AbstractExampleSetWriter {
 			if (isWindowsPlatform()) {
 				Runtime.getRuntime().exec("Rgui " + tmpFile.getAbsolutePath());
 			} else if (isMacOSPlatform()) {
+
+				// transfer the file to the local computer
+				if (!RUtils.getHost().equals("localhost")) {
+					// create another local file  and transfer the workspace file from the rserver
+					REXP xp = connection.parseAndEval("r=readBin('" + tmpFile.getAbsolutePath() + "','raw',2024*2024); unlink('" + tmpFile.getAbsolutePath() + "'); r");
+					FileOutputStream oo = new FileOutputStream(tmpFile);
+					oo.write(xp.asBytes());
+					oo.close();
+				}
+
 				Runtime.getRuntime().exec("open -n -a /Applications/R.app/ " + tmpFile.getAbsolutePath());
 			} else { // linux and the rest of the world
 				Runtime.getRuntime().exec("R " + tmpFile.getAbsolutePath());
@@ -64,23 +76,11 @@ public class OpenInROperator extends AbstractExampleSetWriter {
 		} catch (Throwable e) {
 			throw new RuntimeException("spawning of R-process failed", e);
 		}
-
-		return exampleSet;
 	}
 
 
 	private boolean isMacOSPlatform() {
 		return (System.getProperty("mrj.version") != null);
-	}
-
-
-	@Override
-	public List<ParameterType> getParameterTypes() {
-		List<ParameterType> types = super.getParameterTypes();
-
-		types.add(new ParameterTypeString(OUTPUT_VAR_NAME, "name of the variable of the input in the r-workspace", "exampleSet", true));
-
-		return types;
 	}
 
 
