ceaf5e97d36ff3c2de55ca11443009e7fabf71be
holgerbrandl <holgerbrandl@653ba4c6-162f-11df-b3c2-2971b5c2da17>
2010-02-22 20:11:27 +0000

    added icons

--- trunk/src/de/mpicbg/tds/rm/rplugin/gui/DynamicRPlotRenderer.java
+++ trunk/src/de/mpicbg/tds/rm/rplugin/gui/DynamicRPlotRenderer.java
@@ -30,23 +30,23 @@ import java.util.Map;
 public class DynamicRPlotRenderer extends AbstractRenderer {
 
 
-	@Override
-	public Reportable createReportable(final Object renderable, IOContainer ioContainer, int desiredWidth, int desiredHeight) {
-		return null;
-	}
+    @Override
+    public Reportable createReportable(final Object renderable, IOContainer ioContainer, int desiredWidth, int desiredHeight) {
+        return null;
+    }
 
 
-	@Override
-	public String getName() {
-		return "R Result";
-	}
+    @Override
+    public String getName() {
+        return "R Result";
+    }
 
 
-	@Override
-	public Component getVisualizationComponent(Object renderable, IOContainer ioContainer) {
-		RPlotIOObject exampleSet = (RPlotIOObject) renderable;
+    @Override
+    public Component getVisualizationComponent(Object renderable, IOContainer ioContainer) {
+        RPlotIOObject exampleSet = (RPlotIOObject) renderable;
 
-		//        MediaTracker mediaTracker = new MediaTracker(canvas);
+        //        MediaTracker mediaTracker = new MediaTracker(canvas);
 //        mediaTracker.addImage(image, 0);
 //        try {
 //            mediaTracker.waitForID(0);
@@ -58,139 +58,144 @@ public class DynamicRPlotRenderer extends AbstractRenderer {
 //
 //        canvas.setSize(image.getWidth(null), image.getHeight(null));
 
-		return new RPlotCanvas(exampleSet.getImage(), exampleSet.getScript(), exampleSet.getPushTable());
-	}
+        return new RPlotCanvas(exampleSet.getImage(), exampleSet.getScript(), exampleSet.getPushTable());
+    }
 
 
-	private static class RPlotCanvas extends Canvas {
+    private static class RPlotCanvas extends Canvas {
 
-		private Image image;
-
-
-		public RPlotCanvas(Image image, final String script, final Map<String, IOObject> pushTable) {
-			this.image = image;
-
-			addComponentListener(new ComponentAdapter() {
-				@Override
-				public void componentResized(ComponentEvent e) {
-					if (!isVisible()) {
-						return;
-					}
-
-					if (!Boolean.parseBoolean(System.getProperty(PluginInitializer.RELPOT_ON_RESIZE, "false"))) {
-						return;
-					}
-
-					RPlotCanvas.this.image = recreateImage(pushTable, script);
-				}
-			});
-
-			addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseClicked(MouseEvent mouseEvent) {
-					if (mouseEvent.getClickCount() == 2) {
-						RPlotCanvas.this.image = recreateImage(pushTable, script);
-					}
-				}
-			});
-		}
+        private Image image;
 
 
-		public BufferedImage recreateImage(Map<String, IOObject> pushTable, String script) {
-			RConnection connection = null;
-			try {
-				connection = new RConnection();
+        public RPlotCanvas(Image image, final String script, final Map<String, IOObject> pushTable) {
+            this.image = image;
 
-				RUtils.push2R(connection, pushTable);
+            RPlotCanvas.this.image = recreateImage(pushTable, script);
 
-				BufferedImage bufferedImage = toBufferedImage(RImageFactory.createImage(connection, script, getWidth(), getHeight()));
+            addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    if (!isVisible()) {
+                        return;
+                    }
 
-				connection.close();
+                    if (!Boolean.parseBoolean(System.getProperty(PluginInitializer.RELPOT_ON_RESIZE, "false"))) {
+                        return;
+                    }
 
-				return bufferedImage;
-			} catch (Throwable e1) {
-				if (connection != null) {
-					connection.close();
-				}
-				throw new RuntimeException(e1);
-			}
-		}
+                    RPlotCanvas.this.image = recreateImage(pushTable, script);
+                }
+            });
 
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent mouseEvent) {
+                    if (mouseEvent.getClickCount() == 2) {
+                        RPlotCanvas.this.image = recreateImage(pushTable, script);
 
-		public void paint(Graphics g) {
-			g.drawImage(image, 0, 0, null);
-		}
-
-
-		public static BufferedImage toBufferedImage(Image image) {
-
-			if (image instanceof BufferedImage) {
-				return (BufferedImage) image;
-			}
-
-			// This code ensures that all the pixels in the image are loaded
-			image = new ImageIcon(image).getImage();
-
-			// Determine if the image has transparent pixels
-			boolean hasAlpha = hasAlpha(image);
-
-			// Create a buffered image with a format that's compatible with the
-			// screen
-			BufferedImage bimage = null;
-			GraphicsEnvironment ge = GraphicsEnvironment
-					.getLocalGraphicsEnvironment();
-			try {
-				// Determine the type of transparency of the new buffered image
-				int transparency = Transparency.OPAQUE;
-				if (hasAlpha == true) {
-					transparency = Transparency.BITMASK;
-				}
-
-				// Create the buffered image
-				GraphicsDevice gs = ge.getDefaultScreenDevice();
-				GraphicsConfiguration gc = gs.getDefaultConfiguration();
-				bimage = gc.createCompatibleImage(image.getWidth(null), image
-						.getHeight(null), transparency);
-			} catch (HeadlessException e) {
-			} // No screen
-
-			if (bimage == null) {
-				// Create a buffered image using the default color model
-				int type = BufferedImage.TYPE_INT_RGB;
-				if (hasAlpha == true) {
-					type = BufferedImage.TYPE_INT_ARGB;
-				}
-				bimage = new BufferedImage(image.getWidth(null), image
-						.getHeight(null), type);
-			}
-
-			// Copy image to buffered image
-			Graphics g = bimage.createGraphics();
-
-			// Paint the image onto the buffered image
-			g.drawImage(image, 0, 0, null);
-			g.dispose();
-
-			return bimage;
-		}
+                        invalidate();
+                        repaint();
+                    }
+                }
+            });
+        }
 
 
-		public static boolean hasAlpha(Image image) {
-			// If buffered image, the color model is readily available
-			if (image instanceof BufferedImage) {
-				return ((BufferedImage) image).getColorModel().hasAlpha();
-			}
+        public BufferedImage recreateImage(Map<String, IOObject> pushTable, String script) {
+            RConnection connection = null;
+            try {
+                connection = new RConnection();
 
-			// Use a pixel grabber to retrieve the image's color model;
-			// grabbing a single pixel is usually sufficient
-			PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
-			try {
-				pg.grabPixels();
-			} catch (InterruptedException e) {
-			}
+                RUtils.push2R(connection, pushTable);
 
-			// Get the image's color model
-			return pg.getColorModel().hasAlpha();
-		}
-	}
+                BufferedImage bufferedImage = toBufferedImage(RImageFactory.createImage(connection, script, getWidth(), getHeight()));
+
+                connection.close();
+
+                return bufferedImage;
+            } catch (Throwable e1) {
+                if (connection != null) {
+                    connection.close();
+                }
+                throw new RuntimeException(e1);
+            }
+        }
+
+
+        public void paint(Graphics g) {
+            g.drawImage(image, 0, 0, null);
+        }
+
+
+        public static BufferedImage toBufferedImage(Image image) {
+
+            if (image instanceof BufferedImage) {
+                return (BufferedImage) image;
+            }
+
+            // This code ensures that all the pixels in the image are loaded
+            image = new ImageIcon(image).getImage();
+
+            // Determine if the image has transparent pixels
+            boolean hasAlpha = hasAlpha(image);
+
+            // Create a buffered image with a format that's compatible with the
+            // screen
+            BufferedImage bimage = null;
+            GraphicsEnvironment ge = GraphicsEnvironment
+                    .getLocalGraphicsEnvironment();
+            try {
+                // Determine the type of transparency of the new buffered image
+                int transparency = Transparency.OPAQUE;
+                if (hasAlpha == true) {
+                    transparency = Transparency.BITMASK;
+                }
+
+                // Create the buffered image
+                GraphicsDevice gs = ge.getDefaultScreenDevice();
+                GraphicsConfiguration gc = gs.getDefaultConfiguration();
+                bimage = gc.createCompatibleImage(image.getWidth(null), image
+                        .getHeight(null), transparency);
+            } catch (HeadlessException e) {
+            } // No screen
+
+            if (bimage == null) {
+                // Create a buffered image using the default color model
+                int type = BufferedImage.TYPE_INT_RGB;
+                if (hasAlpha == true) {
+                    type = BufferedImage.TYPE_INT_ARGB;
+                }
+                bimage = new BufferedImage(image.getWidth(null), image
+                        .getHeight(null), type);
+            }
+
+            // Copy image to buffered image
+            Graphics g = bimage.createGraphics();
+
+            // Paint the image onto the buffered image
+            g.drawImage(image, 0, 0, null);
+            g.dispose();
+
+            return bimage;
+        }
+
+
+        public static boolean hasAlpha(Image image) {
+            // If buffered image, the color model is readily available
+            if (image instanceof BufferedImage) {
+                return ((BufferedImage) image).getColorModel().hasAlpha();
+            }
+
+            // Use a pixel grabber to retrieve the image's color model;
+            // grabbing a single pixel is usually sufficient
+            PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+            try {
+                pg.grabPixels();
+            } catch (InterruptedException e) {
+            }
+
+            // Get the image's color model
+            return pg.getColorModel().hasAlpha();
+        }
+    }
 }
